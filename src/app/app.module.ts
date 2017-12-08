9a66713fccf7a26de2f86ec994db16b6107ce011
Ivan Koretskyy <ivan.koretskyy@cgm.com>
2017-12-08 18:42:06 +0200

    add message component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {MatButtonModule} from '@angular/material';
+import {MatCardModule} from '@angular/material/card';
 
 import { AppComponent } from './app.component';
+import { ApiService } from './api.service';
+import { HttpModule} from '@angular/http';
 
+import { MessagesComponent } from './messages.component';
 
 @NgModule({
   declarations: [
     AppComponent
+    , MessagesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    MatButtonModule,
+    MatCardModule
   ],
-  providers: [],
+  providers: [ApiService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
